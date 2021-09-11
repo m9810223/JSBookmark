0015644240ba01e959ee8924091775f23ed92b33
m9810223 <m9810223@gmail.com>
2021-09-11 12:10:14 +0800

    2021/09/11 12:10:12

--- others/pixel.js
+++ others/pixel.js
@@ -2,8 +2,7 @@ const initPixel = (pixel) => {
   console.log('initPixel', pixel);
   if (
     [...document.querySelectorAll('script')].filter(
-      (script) =>
-        script.textContent.includes('fbq') && script.textContent.includes(pixel)
+      (script) => script.textContent.includes('fbq') && script.textContent.includes(pixel)
     ).length
   ) {
     console.log('重複', pixel);
@@ -29,37 +28,67 @@ const initPixel = (pixel) => {
   console.log(pixel, '埋了');
 };
 
+// const ainitPixel = async (pixel) => {
+//   return new Promise((resolve) => {
+//     const checker = setInterval(() => {
+//       console.log('*** checker');
+//       if (window?.fbq?.instance?.pixelsByID[pixel]) {
+//         console.warn('*** pixel ok', pixel);
+//         clearInterval(checker);
+//         resolve();
+//       }
+//       const scriptInstalled = Array.from(
+//         document.querySelectorAll('script'),
+//         (s) => s.textContent
+//       ).filter((t) => t.match(/fbq.{2}init/g) && t.includes(pixel)).length;
+//       if (!scriptInstalled) {
+//         const script = document.createElement('script');
+//         script.innerHTML = `
+//           !function(f,b,e,v,n,t,s)
+//           {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+//           n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+//           if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+//           n.queue=[];t=b.createElement(e);t.async=!0;
+//           t.src=v;s=b.getElementsByTagName(e)[0];
+//           s.parentNode.insertBefore(t,s)}(window, document,'script',
+//           'https://connect.facebook.net/en_US/fbevents.js');
+//           fbq('init', ${pixel});
+//           fbq('track', 'PageView');
+//       `;
+//         document.head.appendChild(script);
+//         console.warn('*** pixel script installed');
+//       }
+//     }, 500);
+//   });
+// };
+
 const ainitPixel = async (pixel) => {
   return new Promise((resolve) => {
-    const checker = setInterval(() => {
-      console.log('*** checker');
-      if (window?.fbq?.instance?.pixelsByID[pixel]) {
-        console.warn('*** pixel ok', pixel);
-        clearInterval(checker);
-        resolve();
-      }
-      const scriptInstalled = Array.from(
-        document.querySelectorAll('script'),
-        (s) => s.textContent
-      ).filter((t) => t.match(/fbq.{2}init/g) && t.includes(pixel)).length;
-      if (!scriptInstalled) {
-        const script = document.createElement('script');
-        script.innerHTML = `
-          !function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', ${pixel});
-          fbq('track', 'PageView');
-      `;
-        document.head.appendChild(script);
-        console.warn('*** pixel script installed');
-      }
-    }, 500);
+    if (window?.fbq?.instance?.pixelsByID[pixel]) {
+      resolve('already installed', pixel);
+    }
+
+    !(function (f, b, e, v, n, t, s) {
+      if (f.fbq) return;
+      n = f.fbq = function () {
+        n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
+      };
+      if (!f._fbq) f._fbq = n;
+      n.push = n;
+      n.loaded = !0;
+      n.version = '2.0';
+      n.queue = [];
+      t = b.createElement(e);
+      t.async = !0;
+      t.src = v;
+      s = b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t, s);
+    })(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
+
+    window.fbq('init', pixel);
+    window.fbq('trackSingle', pixel, 'PageView');
+
+    resolve('install for pta', pixel);
   });
 };
 
